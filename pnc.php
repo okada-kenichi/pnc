9c6266ef52904bd9a1409cbdc38513f46da660c6
okada-kenichi <71626308+okada-kenichi@users.noreply.github.com>
2021-10-15 23:14:39 +0900

    Update pnc.php

--- pnc.php
+++ pnc.php
@@ -8,23 +8,23 @@ if(isset($_GET["pnc"]) && !preg_match('/[^0-9]/', $_GET["pnc"]) && ($_GET["pnc"]
     $param = (int)htmlspecialchars($_GET["pnc"]);
     $arr["status"] = "OK";
     //メイン処理
-    $remainder = 1;
+    $remainder = 1;//初期値として1を代入
     for($i = 2; $i < ceil(sqrt($param)); $i++){//平方根まで調べればいいのでsqrt()でceilにて小数点以下切り上げ
         if($param % $i == 0){
-            $remainder = 0;
+            $remainder = 0;//割り切れたら0を代入
             break;//割り切れた瞬間、合成数であることが確定するのでブレイクしてforを抜ける
         }
-        //$remainderは1のまま
+        //$remainderは1のまま変化せず
+    }
+    
+    if($remainder == 0){
+        $arr["number"] = "Composite number";
+    }else{
+        $arr["number"] = "Prime number";
     }
     $cal = (int)$i - 1;
     $arr["checknumber"] = "{$param}";
     $arr["calculations"] = "{$cal} times";
-    if($remainder == 0){
-        $arr["number"] = "Composite number";
-        
-    }else{
-        $arr["number"] = "Prime number";
-    }
     
 } else {
     // paramの値が不適ならstatusをnoにしてプログラム終了
